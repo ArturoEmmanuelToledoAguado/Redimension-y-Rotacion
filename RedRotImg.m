a3a4bd817dbb9dc94bee831426f6aa1c95109a7c
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-07 18:50:14 -0500

    Fin de la tarea

--- RedRotImg.m
+++ RedRotImg.m
@@ -7,3 +7,30 @@ img=imread('IMG/1.jpg');
 
 %Rotamos 5° en sentido contrario de las manecillas del reloj
 imgRot=imrotate(img,5);
+
+%Dimensiones de la imagen rotada y normal
+[fr cr]=size(imgRot);
+[f c]=size(img);
+
+%Mostramos imagenes
+figure()
+imshow(img)
+title('Imagen Original')
+figure()
+imshow(imgRot)
+title('Imagen Rotada 5°')
+
+%Comparamos
+if (f<fr) || (c<cr)
+    imgr=imresize(img,[fr cr/3]);
+    imgSum=imadd(imgr,imgRot);
+    figure()
+    imshow(imgSum)
+    title('Suma de Original y rotada 5°')
+else
+    imgr=imresize(img,[f c/3]);
+    imgSum=imadd(imgr,imgRot);
+    figure()
+    imshow(imgSum)
+    title('Suma de Original y rotada 5°')
+end
